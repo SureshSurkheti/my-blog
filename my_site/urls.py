cf5f5f9f599d4529033c7b7d659051112e6c367e
suresh surkhet <surekhetisuresh123@gmail.com>
2026-08-26 21:34:03 +0900

    Serve media files in production

--- my_site/urls.py
+++ my_site/urls.py
@@ -1,6 +1,6 @@
 from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
+from django.views.static import serve
 from django.contrib.sitemaps.views import sitemap
 from django.contrib.staticfiles.storage import staticfiles_storage
 from django.http import HttpResponse
@@ -69,4 +69,10 @@ urlpatterns = [
     path("", include("blog.urls")),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [
+    path(
+        f"{settings.MEDIA_URL.lstrip('/')}<path:path>",
+        serve,
+        {"document_root": settings.MEDIA_ROOT},
+    ),
+]
