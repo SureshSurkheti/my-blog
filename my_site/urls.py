6e772a25f279cae220e9cc8743e485309b6fa2aa
suresh surkhet <surekhetisuresh123@gmail.com>
2026-08-26 21:12:50 +0900

    Serve media files in production

--- my_site/urls.py
+++ my_site/urls.py
@@ -69,4 +69,6 @@ urlpatterns = [
     path("", include("blog.urls")),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    # In production the web server serves uploads; Django only does it locally.
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
